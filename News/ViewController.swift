f1cd3ba477dd011b4964df6f48510510376550a8
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-10-29 23:57:56 +0700

    init project

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by TheGIZzz on 29/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
